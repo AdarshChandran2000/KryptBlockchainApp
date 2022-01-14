bda5cf6113e3d675bc6325e1af232de10050ef2d
AdarshChandran2000 <lachikku2210@gmail.com>
2022-01-14 06:00:59 +0300

    Image size updated

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -11,9 +11,9 @@ const TransactionCard = ({addressTo, addressFrom, amount, timestamp, message, ke
     
     return (
         <div className='bg-[#181918] m-4 flex flex-1
-            2xl:min-w-[450px]
-            2xl:max-w-[500px]
-            sm:min-w-[270px]
+        2xl:min-w-[350px]
+        2xl:max-w-[400px]    
+        sm:min-w-[270px]
             sm:max-w-[300px]
             min-w-full
             flex-col p-3 rounded-md hover:shadow-2xl
